aa37fc244fe4ef6011582efb0c1b59777f6eff31
GIRAMISU-CHI <vsingh@is.mpg.de>
2025-09-05 17:15:31 +0200

    Update hr_survey.py

--- hr_survey.py
+++ hr_survey.py
@@ -15,23 +15,23 @@ from firebase_admin import credentials, db
 # Initialize Firebase only once
 @st.cache_resource
 def init_firebase():
-if not firebase_admin._apps:
-cred_dict = {
-"type": os.environ.get("FIREBASE_TYPE"),
-"project_id": os.environ.get("FIREBASE_PROJECT_ID"),
-"private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
-"private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
-"client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
-"client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-"auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-"token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-"auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
-"client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
-}
-cred = credentials.Certificate(cred_dict)
-firebase_admin.initialize_app(cred, {
-'databaseURL': os.environ.get("FIREBASE_DATABASE_URL")
-})
+    if not firebase_admin._apps:
+        cred_dict = {
+        "type": os.environ.get("FIREBASE_TYPE"),
+        "project_id": os.environ.get("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.environ.get("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.environ.get("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n"),
+        "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
+        "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_X509_CERT_URL")
+        }
+        cred = credentials.Certificate(cred_dict)
+        firebase_admin.initialize_app(cred, {
+        'databaseURL': os.environ.get("FIREBASE_DATABASE_URL")
+        })
 
 
 # Always call this before any Firebase operation
